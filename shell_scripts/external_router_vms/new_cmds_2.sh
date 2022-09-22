1e9a941934124c6994501589e85d633858d9e32f
mdsufair <md.sufair@oneconvergence.com>
2022-09-22 08:22:23 -0400

    update existing shell scripts for automation

--- shell_scripts/external_router_vms/new_cmds_2.sh
+++ shell_scripts/external_router_vms/new_cmds_2.sh
@@ -6,10 +6,16 @@ FAB_NO=$4
 EXT_RTR_IP=`ip -o -4 addr | grep eth0 | awk '{print $4}' | awk -F'/' '{print $1}'`
 EXT_RTR_NET=`echo ${EXT_RTR_IP} | cut -d'.' -f1-3`".0"
 NOIRO_CTRLR_IP=172.28.184.8
+# Environment vars to let us access stuff
+HTTP_PROXY='http://proxy.esl.cisco.com:80'
+HTTP_PROXY_STRING="http_proxy=${HTTP_PROXY}"
+HTTPS_PROXY_STRING="https_proxy=${HTTP_PROXY}"
+export ${HTTP_PROXY_STRING}
+export ${HTTPS_PROXY_STRING}
 
 # Be sure you have a good reason for changing these
 # two environment variables
-NOVACLIENT_VERSION=stable/ocata
+NOVACLIENT_VERSION=ocata-eol
 
 # Supported releases
 NEWTON="newton"
@@ -26,7 +32,6 @@ GW1_IP='1.250.1.254'
 GW2_IP='1.251.1.254'
 EXT1_IP='1.250.1.1'
 EXT2_IP='1.251.1.1'
-
 
 # Assume opendev
 GIT_CLONE="git clone https://opendev.org"
@@ -79,15 +84,24 @@ echo "undercloud ip set to ${UNDERCLOUD_IP}."
 sudo -- sh -c "echo 127.0.0.1       $(hostname) >> /etc/hosts"
 sudo -- sh -c "echo 'nameserver 172.28.184.18' >> /etc/resolv.conf"
 sudo -- sh -c "echo 'search noiro.lab nested.lab' >> /etc/resolv.conf"
+# Ensure we only use IPv4 to resolve hostnames - doesn't seem to wwork with IPv6
+sudo -- sh -c "echo 'Acquire::ForceIPv4 \"true\";' >> /etc/apt/apt.conf.d/99only-ipv4"
 sudo -E apt-get update
+# Get the new "Let's Encrypt" certificate, since the one we have has expired
+sudo -E apt-get install apt-transport-https ca-certificates -y ; sudo update-ca-certificates
 
 # We need python2.7 for OpenStack
 sudo -E apt-get -y --allow-unauthenticated install python2.7
 sudo -E apt-get -y --allow-unauthenticated install sshpass
 sudo -E apt-get -y --allow-unauthenticated install git
 sudo -E apt-get -y --allow-unauthenticated install jq
+sudo -E apt-get -y --allow-unauthenticated install vim
 
-FAB_NAME=$(curl -X GET http://noirolab.cisco.com:5000/api/fabric/${FAB_NO} | jq -r .name)
+# Fix certs
+sudo apt-get install apt-transport-https ca-certificates -y
+sudo update-ca-certificates
+openssl s_client -showcerts -servername $(hostname).noiro.lab -connect $(hostname).noiro.lab:443 </dev/null 2>/dev/null | sed -n -e '/BEGIN\ CERTIFICATE/,/END\ CERTIFICATE/ p'  > git-mycompany-com.pem
+cat git-mycompany-com.pem | sudo tee -a /etc/ssl/certs/ca-certificates.crt
 
 
 if [ "${UNDERCLOUD_TYPE}" = "${DIRECTOR}" ]; then
@@ -120,7 +134,7 @@ if [ "${UNDERCLOUD_TYPE}" = "${DIRECTOR}" ]; then
     echo "scp -o StrictHostKeyChecking=no ${RCFILE} ${OVERCLOUD_USER}@\$CIP: " >> test.sh
     echo "NH_IP=\`ssh -o StrictHostKeyChecking=no ${OVERCLOUD_USER}@\${CIP} \"sudo ifconfig ext-br\" | grep 'inet ' | awk '{print \$2}'\`" >> test.sh
     echo "echo \"sudo route add -host \$CIP gateway \$NH_IP\" >> routes.sh" >> test.sh
-    if [ "${FAB_NO}" = "202" ]; then
+    if [ "$1" = "${TRAIN}" -o "${RELEASE_FILE}" = "${TRAIN}" ]; then
         echo "ssh -o StrictHostKeyChecking=no ${OVERCLOUD_USER}@\$CIP \"sudo iptables -I INPUT 4 -s 10.30.120.0/24 -p tcp -m multiport --dports 22 -m state --state NEW -m comment --comment '003 accept ssh from ctlplane subnet 10.30.120.0/24 ipv4' -j ACCEPT\"" >> test.sh
         echo "ssh -o StrictHostKeyChecking=no ${OVERCLOUD_USER}@\$CIP \"sudo iptables -I INPUT 5 -s 1.250.1.0/24 -p tcp -m multiport --dports 22 -m state --state NEW -m comment --comment '003 accept ssh from ctlplane subnet 1.250.1.0/24 ipv4' -j ACCEPT\"" >> test.sh
     fi
@@ -143,13 +157,8 @@ else
     CLOUD_GATEWAY=${UNDERCLOUD_IP}
 fi
 
-# Environment vars to let us access stuff
-HTTP_PROXY='http://proxy.esl.cisco.com:80'
-HTTP_PROXY_STRING="http_proxy=${HTTP_PROXY}"
-HTTPS_PROXY_STRING="https_proxy=${HTTP_PROXY}"
+# Set no-proxy
 NO_PROXY_STRING="no_proxy=127.0.0.1,localhost,${EXT_RTR_IP},${CLOUD_GATEWAY},${EXT_RTR_NET}/24,${CLOUD_NET}/24,${NOIRO_CTRLR_IP}"
-export ${HTTP_PROXY_STRING}
-export ${HTTPS_PROXY_STRING}
 export ${NO_PROXY_STRING}
 export PYTHONPATH=/home/noiro/noirotest
 echo "export ${HTTP_PROXY_STRING}" >> .bashrc
@@ -162,18 +171,18 @@ echo "export PYTHONPATH=/home/noiro/noirotest" >> .bashrc
 # (note: this is needed so we can get the keystone IP below)
 sudo route add -net ${CLOUD_NET} netmask 255.255.255.0 gateway ${CLOUD_GATEWAY}
 
+# The controllers are missing a route back to the external router VM.
 if [ "${UNDERCLOUD_TYPE}" = "${DIRECTOR}" ]; then
     # Set up env vars for access
     CTRLR_INT_IP=`ssh -o StrictHostKeyChecking=no ${UNDERCLOUD_USER}@${UNDERCLOUD_IP} "source stackrc && nova list | grep controller-0" | awk -F'|' '{print $7}' | cut -c11- | tr -d '[:space:]'`
-    KEYSTONE_IP=`ssh -o StrictHostKeyChecking=no ${OVERCLOUD_USER}@${CTRLR_INT_IP} "sudo egrep auth_url /etc/neutron/neutron.conf"  | grep v3 | awk -F':' '{print $2}' | cut -c3-`
-    MGMT_VLAN=$(ssh o StrictHostKeyChecking=no ${OVERCLOUD_USER}@${CTRLR_INT_IP} "sudo netstat -nr" | awk '{print $1}' | grep -m 1 1.121 | cut -d "." -f 3)
+    KEYSTONE_IP=`ssh -o StrictHostKeyChecking=no ${OVERCLOUD_USER}@${CTRLR_INT_IP} "sudo egrep auth_url /var/lib/config-data/neutron/etc/neutron/neutron.conf"  | head -n 1 | awk -F':' '{print $2}' | cut -c3-`
+    MGMT_VLAN=$(ssh -o StrictHostKeyChecking=no ${OVERCLOUD_USER}@${CTRLR_INT_IP} "sudo netstat -nr" | awk '{print $1}' | grep -m 1 1.121 | cut -d "." -f 3)
     for host in $(cat routes.sh | awk '{print $5}'); do
-        ssh -o StrictHostKeyChecking=no ${OVERCLOUD_USER}@$host "sudo route add -net ${GW1_IP} netmask 255.255.255.0 gateway 1.121.${MGMT_VLAN}.1 dev vlan${MGMT_VLAN}".  
+	ssh -o StrictHostKeyChecking=no ${OVERCLOUD_USER}@$host "sudo route add -net ${EXT1_IP::-1}0 netmask 255.255.255.0 gateway 1.121.${MGMT_VLAN}.1 dev vlan${MGMT_VLAN}"
     done
-    sudo route add -net 1.121.${MGMT_VLAN}.0 netmask 255.255.255.0 gateway ${GW1_IP}
     ssh -o StrictHostKeyChecking=no ${OVERCLOUD_USER}@${CTRLR_INT_IP} "sudo cp ~/${RCFILE} /root/" >> /dev/null
     # Establish ssh logins
-    for CIP in $(source stackrc && nova list | grep controller | awk -F'|' '{print $7}' | cut -c11-); do 
+    for CIP in $(ssh -o StrictHostKeyChecking=no ${UNDERCLOUD_USER}@${UNDERCLOUD_IP} "source stackrc && nova list | grep controller" | awk -F'|' '{print $7}' | cut -c11-); do 
         ssh -o StrictHostKeyChecking=no ${OVERCLOUD_USER}@$CIP ls >> /dev/null
     done
     ssh -o StrictHostKeyChecking=no ${OVERCLOUD_USER}@${CTRLR_REST_IP} ls >> /dev/null
@@ -186,6 +195,7 @@ if [ "${UNDERCLOUD_TYPE}" = "${DIRECTOR}" ]; then
     VIP_CTRLR_IP=$(ssh -o StrictHostKeyChecking=no ${UNDERCLOUD_USER}@${UNDERCLOUD_IP} "nova list" | grep ${VIP_CTRLR_NAME} | awk -F'=' '{print $2}')
     CTRLR_INT_IP_NH=$(ssh -o StrictHostKeyChecking=no ${OVERCLOUD_USER}@${CTRLR_INT_IP} "sudo ifconfig ext-br" | grep 'inet ' | awk '{print $2}')
     sudo route add -host ${VIP_CTRLR_IP} gateway ${CTRLR_INT_IP_NH}
+    sudo route add -net 1.121.${MGMT_VLAN}.0 netmask 255.255.255.0 gateway ${GW1_IP} dev eth1
 fi
 
 
@@ -289,10 +299,10 @@ fi
 sed -i "s/no_proxy=,/no_proxy=\$no_proxy,/g" ~/${RCFILE}
 
 # Download and install rally
-wget -q -O- https://raw.githubusercontent.com/noironetworks/rally/noiro-master/install_rally.sh > install_rally.sh
+wget -q -O- https://raw.githubusercontent.com/noironetworks/rally/dualstack-vcpe/install_rally.sh > install_rally.sh
 # Get rid of question
 sed -i 's/ask_yn "Proceed with installation anyway?"/true/g' ~/install_rally.sh
-sed -i 's/bootstrap.pypa.io/bootstrap.pypa.io\/2.7/g' ~/install_rally.sh
+sed -i 's/bootstrap.pypa.io/bootstrap.pypa.io\/pip\/2.7/g' ~/install_rally.sh
 chmod +x ./install_rally.sh
 ./install_rally.sh
 . ~/${RCFILE}
@@ -308,7 +318,7 @@ cp ~/rally/src/rally-jobs/extra/install_benchmark.sh ~/.rally/extra
 # newton-eol version of python-novaclient (GBP workflow/tests),
 # so we use a more recent version
 ${GIT_CLONE}/openstack/python-novaclient.git
-cd python-novaclient && git checkout $NOVACLIENT_VERSION && cd
+cd python-novaclient && git checkout $RELEASE && cd
 sudo -E pip install python-novaclient/
 
 sudo -E pip install openstacksdk==0.14.0
@@ -380,14 +390,27 @@ if [ "${UNDERCLOUD_TYPE}" = "${DIRECTOR}" ]; then
     ssh -o StrictHostKeyChecking=no ${OVERCLOUD_USER}@${CTRLR_INT_IP} "${AIM_CMD_PREFIX} aimctl manager physical-domain-delete pdom_physnet1" >> /dev/null
     ssh -o StrictHostKeyChecking=no ${OVERCLOUD_USER}@${CTRLR_INT_IP} "${AIM_CMD_PREFIX} aimctl manager load-domains" >> /dev/null
 fi
+if [ "${UNDERCLOUD_TYPE}" = "${DIRECTOR}" ]; then
+    if [ "$1" = "${QUEENS}" -o "${RELEASE_FILE}" = "${QUEENS}" ]; then
+	ssh -o StrictHostKeyChecking=no ${UNDERCLOUD_USER}@${UNDERCLOUD_IP} "tar -xvzf openstack-ciscorpms-repo-* ./python-gbp*"
+	ssh -o StrictHostKeyChecking=no ${UNDERCLOUD_USER}@${UNDERCLOUD_IP} "scp python-gbp* ${OVERCLOUD_USER}@${CTRLR_INT_IP}:~"
+	ssh -o StrictHostKeyChecking=no ${OVERCLOUD_USER}@${CTRLR_INT_IP} "sudo yum install python-gbp* -y"
+    fi
+    if [ "$1" = "${TRAIN}" -o "${RELEASE_FILE}" = "${TRAIN}" ]; then
+	ssh -o StrictHostKeyChecking=no ${UNDERCLOUD_USER}@${UNDERCLOUD_IP} "tar -xvzf openstack-ciscorpms-repo-* ./python3-gbp*"
+	ssh -o StrictHostKeyChecking=no ${UNDERCLOUD_USER}@${UNDERCLOUD_IP} "scp python3-gbp* ${OVERCLOUD_USER}@${CTRLR_INT_IP}:~"
+	ssh -o StrictHostKeyChecking=no ${OVERCLOUD_USER}@${CTRLR_INT_IP} "sudo yum install python3-gbp* -y"
+    fi
+fi
+
 sudo -E pip install funcsigs
 sudo -E pip install unicodecsv
 sudo -E pip install oslo.db==7.0.0
 sudo -E pip install python-cinderclient==3.5.0
-sudo -E pip install python-openstackclient/
+#sudo -E pip install python-openstackclient/
 sudo -E pip install oslo.config==7.0.0
 sudo -E pip install oslo.log==3.45.2
-sudo -E pip install python-novaclient/
+#sudo -E pip install python-novaclient/
 sudo -E pip install ddt==1.3.1
 if [ "$1" = "${NEWTON}" -o "${RELEASE_FILE}" = "${NEWTON}" ]; then
     sudo -E pip install oslo.utils==4.1.1
@@ -410,3 +433,13 @@ if [ "$1" = "${NEWTON}" -o "${RELEASE_FILE}" = "${NEWTON}" ]; then
     sudo -E pip install oslo.service==2.1.1
     sudo -E pip install oslo.versionedobjects==2.0.2
 fi
+#if [ "$1" = "${QUEENS}" -o "${RELEASE_FILE}" = "${QUEENS}" ]; then
+sudo -E pip install openstacksdk==0.14.0
+cd /home/noiro/
+sudo -E pip install python-openstackclient/
+sudo -E pip install python-novaclient/
+#fi
+#if [ "$1" = "${TRAIN}" -o "${RELEASE_FILE}" = "${TRAIN}" ]; then
+#    IPTABLES_COMMENT="004 accept ssh from ctlplane subnet ${HOST_CIDR}/24 ipv4"
+#    cmd="iptables -I INPUT 4 -s ${HOST_CIDR}/24 -p tcp -m multiport --dports 22 -m state --state NEW -m comment --comment ${IPTABLES_COMMENT} -j ACCEPT"
+#fi
