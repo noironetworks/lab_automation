5285a73168c0685bb8552f8476e1d4cc981a6feb
Iftikhar Rathore <irathore@cisco.com>
2023-11-02 13:11:57 -0700

    Fixed proxy and added pip

--- external_router/base_vm.sh
+++ external_router/base_vm.sh
@@ -1,10 +1,26 @@
+#### Script for creating exetrtr base VM ####
+#
+# Please make sure proxy for apt is configured by creating file
+# /etc/apt/apt.conf.d/99proxy  with content
+# Acquire {
+#   HTTP::proxy "http://proxy.esl.cisco.com:80/";
+#   HTTPS::proxy "http://proxy.esl.cisco.com:80/";
+# }
+#
+#
+
 sudo apt update
 sudo apt upgrade
 sudo apt install sshpass
 sudo apt install python3-neutronclient
 sudo apt install python-is-python3
+sudo apt -y install python3-pip
 sudo apt install python3-venv
 
+export HTTPS_PROXY=http://proxy.esl.cisco.com:80/
+export HTTP_PROXY=http://proxy.esl.cisco.com:80/
+export https_proxy=http://proxy.esl.cisco.com:80/
+export http_proxy=http://proxy.esl.cisco.com:80/
 
 python3 -m venv .venv
 . .venv/bin/activate
